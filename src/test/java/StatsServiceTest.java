1d6dacbff7d5b99df8d27aba4cb511e84faf3b5b
Jul1nh0 <108277816+Jul1nh0@users.noreply.github.com>
2022-09-26 21:36:18 +0300

    Homework

---
+++ src/test/java/StatsServiceTest.java
@@ -0,0 +1,74 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+
+public class StatsServiceTest {
+    @Test
+    public void testSum() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.sum(sales);
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;//sum = 180
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testAvg() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.average(sales);//avg = 15
+        long expected = 15;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMinMonth(){
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int actual = service.minSales(sales);
+        int expected = 9;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMaxMonth(){
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int actual = service.maxSales(sales);
+        int expected = 8;
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void testBelowAvg() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int actual = service.countBelowAvg(sales);//avg = 15
+        long expected = 5;
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void testOverAvg() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int actual = service.countOverAvg(sales);//avg = 15
+        long expected = 5;
+
+        assertEquals(expected, actual);
+    }
+
+}
